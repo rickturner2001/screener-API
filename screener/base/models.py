dbd3f651af909f665489395f07b0f1645cdf21d3
rickturner01 <rickturner2001@gmail.com>
2022-06-20 00:47:02 -0400

    👤Added user registration

--- screener/base/models.py
+++ screener/base/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import User
 class WatchList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.TextField()
+    tickers = models.TextField(default="")
