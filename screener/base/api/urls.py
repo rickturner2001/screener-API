21491ff2356b7cd0599299d4817a073d48a28448
rickturner01 <rickturner2001@gmail.com>
2022-06-20 00:48:52 -0400

    📋Added watchlists

--- screener/base/api/urls.py
+++ screener/base/api/urls.py
@@ -9,8 +9,10 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("", views.get_routes),
     path("watchlists/", views.get_watchlists),
+    path("add_watchlists/", views.add_watchlists),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('market-data/general', views.get_general_market_data),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('tickers-info/', views.tickers_info),
 ]
--- screener/base/api/views.py
+++ screener/base/api/views.py
@@ -1,3 +1,4 @@
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -8,7 +9,9 @@ from base.api.market_data.api_requests import general_market_data_request
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 from rest_framework import generics
-from rest_framework.permissions import  AllowAny
+from rest_framework.permissions import AllowAny
+import pandas as pd
+from base.api.market_data.config import file_path
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -58,7 +61,42 @@ def get_watchlists(request):
     return Response(serializer.data)
 
 
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def add_watchlists(request):
+    # user = request.user
+    # watchlists = user.watchlist_set.all()
+    serializer = WatchlistSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        print(Response(serializer.data))
+        return Response(serializer.data)
+
+    # return Response({"status": "Success"})
+
+
 @api_view(["GET"])
 # @permission_classes([IsAuthenticated])
 def get_general_market_data(request):
     return Response(general_market_data_request())
+
+
+@api_view(["GET"])
+def tickers_info(request):
+    df = pd.read_csv(file_path / "sp500.csv")
+    tickers = df['Symbol'].values
+    securities = df['Security'].values
+    sec_filings = df['SEC filings']
+    sectors = df['GICS Sector']
+    sub_industries = df['GICS Sub-Industry']
+    headquarters = df['Headquarters Location']
+    return Response({
+       ticker: {
+           "security": securities[i],
+           "sec_filings": sec_filings[i],
+           "sector": sectors[i],
+           "sub_industry": sub_industries[i],
+           "headquarter": headquarters[i]
+       } for i, ticker in enumerate(tickers)
+    })
+
